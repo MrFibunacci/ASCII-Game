25e0fae98af2d7f5a71a7929fbfd98125ec9ad83
Unknown <mrfibunacci@gmail.com>
2018-01-30 23:22:35 +0100

    Draw legend added

--- src/Engine/Game/Game.js
+++ src/Engine/Game/Game.js
@@ -20,6 +20,7 @@ class Game {
     draw() {
         this.clearCanvases();
         this.World.renderWorld(this.ctx, new Array(this.Player, new Actor("+", "#333333", 3, 3), new Actor("<", "#00FF00", 5, 3)));
+        this.drawLegend();
     }
 
     update() {
@@ -41,5 +42,40 @@ class Game {
                 break;
             }
         }
+    }
+
+    drawLegend() {
+        this.ctxLegend.font = "20px Arial";
+        this.ctxLegend.fillStyle = "#000";
+        this.ctxLegend.fillText("Contols: ", 5, 20);
+
+        this.ctxLegend.font = "16px Arial";
+        this.ctxLegend.fillText("ArrowKeys to move", 5, 36);
+        this.ctxLegend.fillText("NUMpad 0 to hit", 5, 52);
+        this.ctxLegend.fillText("ctrl to Interact", 5, 68);
+
+        this.ctxLegend.font = "20px Arial";
+        this.ctxLegend.fillStyle = "#000";
+        this.ctxLegend.fillText("Legend: ", 5, 150);
+
+        this.ctxLegend.font = "16px Arial";
+        this.ctxLegend.fillStyle = this.Player.color;
+        this.ctxLegend.fillText(this.Player.appearance, 5, 166);
+        this.ctxLegend.fillStyle = "#000000";
+        this.ctxLegend.fillText(" - You" , 25, 166);
+        this.ctxLegend.fillText("#", 5, 182);
+        this.ctxLegend.fillText(" - Walls", 25, 182);
+        this.ctxLegend.fillText(".", 5, 198);
+        this.ctxLegend.fillText(" - empty room", 25, 198);
+        this.ctxLegend.fillText(" ", 5, 214);
+        this.ctxLegend.fillText(" - nothing", 25, 214);
+        //this.ctxLegend.fillStyle = enemie.color;
+        //this.ctxLegend.fillText(enemie.appearance, 5, 230);
+        //this.ctxLegend.fillStyle = "#000000";
+        //this.ctxLegend.fillText(" - enemies", 25, 230);
+        //this.ctxLegend.fillStyle = healPotion.color;
+        //this.ctxLegend.fillText(healPotion.appearance, 5, 246);
+        //this.ctxLegend.fillStyle = "#000000";
+        //this.ctxLegend.fillText(" - Heal Potion", 25, 246);
     }
 }
